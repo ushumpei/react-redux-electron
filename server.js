d464fdfad06b10e62b0d32e8e063460937303074
ushumpei <shumpei.uzawa@gmail.com>
2016-07-06 12:10:30 +0900

    Set server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(3000);
