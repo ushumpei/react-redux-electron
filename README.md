84903d07862d4a0aa0910512718bae01254e55b6
ushumpei <shumpei.uzawa@gmail.com>
2016-07-10 22:58:48 +0900

    Add README

---
+++ README.md
@@ -0,0 +1,24 @@
+# react, redux electron
+
+I used [todos](https://github.com/reactjs/redux/tree/master/examples/todos) for reference.
+
+## install
+
+clone this repository and,
+
+```
+npm install
+npm run webpack
+```
+
+## Run as server application
+
+```
+npm run server
+```
+
+## Run as desktop application
+
+```
+npm run electron
+```
--- main.js
+++ main.js
@@ -10,13 +10,10 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({width: 300, height: 300});
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/app/static/index.html`);
-
-  // Open the DevTools.
-  win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
